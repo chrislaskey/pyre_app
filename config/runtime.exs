e0f5b92c2cc2c9bea0b36912110fb051c06cdc42
Chris Laskey <contact@chrislaskey.com>
2026-04-03 18:12:42 -0400

    Fix runtime config error

--- config/runtime.exs
+++ config/runtime.exs
@@ -91,7 +91,7 @@ if config_env() == :prod do
 
   config :app, App.Repo,
     database: database_path,
-    pool_size: env!("POOL_SIZE", :integer, "5")
+    pool_size: env!("POOL_SIZE", :integer, 5)
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
@@ -112,11 +112,8 @@ if config_env() == :prod do
   config :app, AppWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: env!("PORT", :integer, 4000)
     ],
     secret_key_base: secret_key_base
 
